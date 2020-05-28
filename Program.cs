f03986467c6ab654fcc7709c58bf2906c374eb19
LeiYangGH <675686066@qq.com>
2020-05-28 23:52:45 +0800

    can judge chnnel

--- Program.cs
+++ Program.cs
@@ -4,40 +4,102 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace MergeWavHeader
 {
     class Program
     {
+        static int Diff(int a, int b)
+        {
+            if (a == 8 && b == 0 || a == 0 && b == 8)
+                return 1;
+            else return Math.Abs(a - b);
+        }
+        static bool IsDoubleChannel(string fileName)
+        {
+            byte[] bytes = File.ReadAllBytes(fileName);
+            int secLen = 8;
+            int secLenH = secLen * 2;
+
+            int i = 0;
+            while (i < bytes.Length / secLen - 1 && i <= 3)
+            {
+                byte[] bsSec = bytes.Skip(i * secLen).Take(secLen).ToArray();
+                int[] bsSecH = new int[16];
+                for (int k = 0; k < 8; k++)
+                {
+                    bsSecH[k * 2] = bsSec[k] / secLenH;
+
+                    bsSecH[k * 2 + 1] = bsSec[k] % secLenH;
+                }
+                Console.WriteLine(string.Join(" ", bsSecH.Select(x => x.ToString("X"))));
+                bool isdouble = true;
+                bool allSame = true;
+                for (int j = 0; j < secLen; j++)
+                {
+                    Console.WriteLine(j);
+                    if (bsSecH[j] != bsSecH[j + secLen])
+                        allSame = false;
+                    if (Diff(bsSecH[j], bsSecH[j + secLen]) > 3)
+                    {
+                        isdouble = false;
+                        break;
+                    }
+                }
+                i++;
+                if (!allSame)
+                {
+                    return isdouble;
+                }
+            }
+            return false;
+        }
         static void Main(string[] args)
         {
             if (DateTime.Now > new DateTime(2020, 5, 30))
                 return;
+            Console.WriteLine("版本2349");
             try
             {
                 string outDir = "已合并";
                 if (!Directory.Exists(outDir))
                     Directory.CreateDirectory(outDir);
-                string headerFile = "5.a";
+
+
                 foreach (string f in Directory.GetFiles(Environment.CurrentDirectory, "*.wav"))
                 {
+                    string headerFile = "5.a";
+                    string extSuffix = "-GG.wav";
                     Console.WriteLine(f);
+                    if (IsDoubleChannel(f))
+                    {
+                        Console.WriteLine($"双声道");
+                        headerFile = "6.a";
+                        extSuffix = "-sh.wav";
+                    }
+                    else
+                    {
+                        Console.WriteLine($"单声道");
+                    }
                     string filename = Path.GetFileName(f);
-                    string outFileName = Path.Combine(outDir, filename.Replace(".wav", "-GG.wav"));
+                    bool isDouble = IsDoubleChannel(f);
+
+
+                    string outFileName = Path.Combine(outDir, filename.ToLower().Replace(".wav", extSuffix));
                     using (var outputStream = File.Create(outFileName))
                     {
                         using (var inputStream = File.OpenRead(headerFile))
                         {
-                            // Buffer size can be passed as the second argument.
                             inputStream.CopyTo(outputStream);
                         }
                         using (var inputStream = File.OpenRead(f))
                         {
-                            // Buffer size can be passed as the second argument.
                             inputStream.CopyTo(outputStream);
                         }
                         Console.WriteLine($"已生成{outFileName}");
                     }
+                    Console.WriteLine();
                 }
             }
             catch (Exception ex)
